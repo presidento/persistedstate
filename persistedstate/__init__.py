891889f19f819ca725e666f3bf158e1fa834afb5
Mate Farkas <Mate.Farkas@oneidentity.com>
2023-02-16 11:07:35 +0100

    Use cache attribute instead of putting everything directly to object attributes

--- persistedstate/__init__.py
+++ persistedstate/__init__.py
@@ -7,11 +7,12 @@ from typing import Any, Optional
 class PersistedState:
     _VACUUM_ON_CHANGE = 2000
 
-    def __init__(self, filepath: os.PathLike, defaults: Optional[dict] = None):
+    def __init__(self, _filepath: os.PathLike, **defaults):
         self.__change_count = 0
-        self.__filepath = pathlib.Path(filepath)
+        self.__filepath = pathlib.Path(_filepath)
         self.__filepath.touch()
         self.__file = self.__filepath.open("r+", encoding="utf-8")
+        self.__cache = {}
         self.__load()
         if defaults:
             for key, value in defaults.items():
@@ -21,14 +22,12 @@ class PersistedState:
     def __load(self) -> None:
         for line in self.__file:
             key, _, value_str = line.partition(":")
-            object.__setattr__(self, key.strip(), json.loads(value_str))
+            self.__cache[key.strip()] = json.loads(value_str)
 
     def __vacuum(self) -> None:
         tmp_file = self.__filepath.with_suffix(self.__filepath.suffix + ".tmp")
         with tmp_file.open("w", encoding="utf-8") as out_file:
-            for key in sorted(dir(self)):
-                if key.startswith("_"):
-                    continue
+            for key in sorted(self.__cache.keys()):
                 self.__write_line(out_file, key)
         self.__file.close()
         tmp_file.replace(self.__filepath)
@@ -36,13 +35,17 @@ class PersistedState:
 
     def __write_line(self, file, key):
         file.write(f"{key}: ")
-        file.write(json.dumps(getattr(self, key)))
+        file.write(json.dumps(self.__cache[key]))
         file.write("\n")
 
+    def __getattr__(self, __name):
+        return self.__cache[__name]
+
     def __setattr__(self, __name: str, __value: Any) -> None:
-        object.__setattr__(self, __name, __value)
         if __name.startswith("_"):
+            object.__setattr__(self, __name, __value)
             return
+        self.__cache[__name] = __value
         self.__write_line(self.__file, __name)
         self.__change_count += 1
         if self.__change_count >= self._VACUUM_ON_CHANGE:
