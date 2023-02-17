c3914ea80debc9aa59bbf75acbe35b8e997cc75e
Mate Farkas <Mate.Farkas@oneidentity.com>
2023-02-17 08:41:31 +0100

    Add supported Python versions to classifiers

--- setup.py
+++ setup.py
@@ -18,6 +18,11 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Topic :: Utilities",
         "Typing :: Typed",
     ]
