b606d117e2f2f5d75e43100292cd352138b96eaf
Mate Farkas <Mate.Farkas@oneidentity.com>
2023-02-17 11:20:52 +0100

    VERSION 23.2
    Add long description to setup.py

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+# 23.2
+
+- Add long description to setup.py
+
 # 23.1
 
 - Initial public (beta) version
--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
+import pathlib
 import setuptools
 
 setuptools.setup(
     name="persistedstate",
-    version="23.1",
+    version="23.2",
     scripts=[],
     author="Máté Farkas",
     author_email="fm@farkas-mate.hu",
     description="Persist state in editable text file",
+    long_description=pathlib.Path("README.md").read_text(encoding="utf-8"),
     long_description_content_type="text/markdown",
     url="https://github.com/presidento/persistedstate",
     packages=["persistedstate"],
@@ -25,5 +27,5 @@ setuptools.setup(
         "Programming Language :: Python :: 3.11",
         "Topic :: Utilities",
         "Typing :: Typed",
-    ]
+    ],
 )
