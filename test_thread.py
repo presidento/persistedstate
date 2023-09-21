6a7a39baf37fac2c68d467b86574f178b8d3a003
Mate Farkas <Mate.Farkas@oneidentity.com>
2023-09-21 19:22:20 +0200

    Extend thread testing

--- test_thread.py
+++ test_thread.py
@@ -1,26 +1,38 @@
-import pathlib
 import concurrent.futures
+import pathlib
+import shutil
+import threading
 
 from src.persistedstate import PersistedState
 
-NUM_OF_THREADS = 128
+NUM_OF_THREADS = 100
+STEPS_OF_EACH_THREAD = 10
+STATE_FILE = pathlib.Path("tmp/threads.state")
+TEMP_FILE = STATE_FILE.with_suffix(".tmp")
+
 
 class TestThreads:
     def setup_method(self):
-        self.filepath = pathlib.Path("tmp/threads.state")
+        self.filepath = STATE_FILE
         self.filepath.unlink(missing_ok=True)
         self.state = None
 
     def test_thread_usage(self):
         self.open_state()
-        with concurrent.futures.ThreadPoolExecutor(max_workers=NUM_OF_THREADS) as executor:
-            futures = [executor.submit(self.threaded_function) for _ in range(NUM_OF_THREADS)]
+        with concurrent.futures.ThreadPoolExecutor(
+            max_workers=NUM_OF_THREADS
+        ) as executor:
+            futures = [
+                executor.submit(self.threaded_function) for _ in range(NUM_OF_THREADS)
+            ]
             concurrent.futures.wait(futures)
+        shutil.copy(STATE_FILE, TEMP_FILE)
         self.close_state()
+        STATE_FILE.unlink()
+        shutil.copy(TEMP_FILE, STATE_FILE)
 
         self.open_state()
-        assert self.state.index == NUM_OF_THREADS
-        assert list(self.state.list) == [i + 1 for i in range(NUM_OF_THREADS)]
+        assert len(self.state.list) == NUM_OF_THREADS * STEPS_OF_EACH_THREAD
         self.close_state()
 
     def open_state(self):
@@ -30,7 +42,5 @@ class TestThreads:
         self.state.close()
 
     def threaded_function(self):
-        with self.state._thread_lock:
-            self.state.index += 1
-            self.state.list.append(self.state.index)
-
+        for _ in range(STEPS_OF_EACH_THREAD):
+            self.state.list.append(threading.get_ident())
